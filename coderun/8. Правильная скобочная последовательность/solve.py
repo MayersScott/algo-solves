d9c59f24851fa9c73c84b1fde87693a33e032c61
MayersScott <148715834+MayersScott@users.noreply.github.com>
2026-02-07 09:28:05 +0300

    Solve CodeRun - #8 Правильная скобочная последовательность

---
+++ coderun/8. Правильная скобочная последовательность/solve.py
@@ -0,0 +1,55 @@
+import sys
+
+def isValid(s: str) -> bool:
+        stack = list()
+
+        opens_brackets = {
+                          '(' : 0,
+                          '[' : 0, 
+                          '{' : 0,
+                         }
+
+        closes_brackets = {
+                          ')' : 0,
+                          ']' : 0, 
+                          '}' : 0,
+                         }
+        for i in s:
+            if i in opens_brackets:
+                opens_brackets[i] += 1
+            elif i in closes_brackets:
+                closes_brackets[i] += 1
+
+        if opens_brackets["("] != closes_brackets[")"]:
+            return False
+        elif opens_brackets["["] != closes_brackets["]"]:
+            return False
+        elif opens_brackets["{"] != closes_brackets["}"]:
+            return False
+
+        for i in s:
+            if i in opens_brackets:
+                stack.append(i)
+            elif i in closes_brackets and len(stack) > 0:
+                if stack[-1] == "(" and i != ")":
+                    return False
+                elif stack[-1] == "[" and i != "]":
+                    return False
+                elif stack[-1] == "{" and i != "}":
+                    return False
+                else:
+                    stack.pop()
+
+        if len(stack) > 0:
+            return False    
+            
+        return True
+
+
+def main():
+    s = input()
+    print("yes" if isValid(s=s) else "no")
+
+
+if __name__ == '__main__':
+    main()
